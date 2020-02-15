42b9eade10db01926ae2ea52864839f124af4894
Lucky Lazy <michaelkevin47@gmail.com>
2020-02-16 01:04:22 +0700

    fixing x

--- index.js
+++ index.js
@@ -159,16 +159,16 @@
      */
     Runner.spriteDefinition = {
         LDPI: {
-            CACTUS_LARGE: { x: 332, y: 2 },
-            CACTUS_SMALL: { x: 228, y: 2 },
-            CLOUD: { x: 86, y: 2 },
-            HORIZON: { x: 2, y: 54 },
-            MOON: { x: 484, y: 2 },
-            PTERODACTYL: { x: 134, y: 2 },
-            RESTART: { x: 2, y: 2 },
-            TEXT_SPRITE: { x: 655, y: 2 },
-            TREX: { x: 848, y: 2 },
-            STAR: { x: 645, y: 2 }
+            CACTUS_LARGE: { x: 335, y: 2 },
+            CACTUS_SMALL: { x: 231, y: 2 },
+            CLOUD: { x: 89, y: 2 },
+            HORIZON: { x: 5, y: 54 },
+            MOON: { x: 487, y: 2 },
+            PTERODACTYL: { x: 137, y: 2 },
+            RESTART: { x: 5, y: 2 },
+            TEXT_SPRITE: { x: 658, y: 2 },
+            TREX: { x: 851, y: 2 },
+            STAR: { x: 648, y: 2 }
         },
         HDPI: {
             CACTUS_LARGE: { x: 652, y: 2 },
